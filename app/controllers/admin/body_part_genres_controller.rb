d8c6f69f60cef41bd469f03d9bd0089159a77e86
Kirra08 <takahito12rai@gmail.com>
2022-06-08 07:00:11 +0000

    ジャンル作成機能

--- app/controllers/admin/body_part_genres_controller.rb
+++ app/controllers/admin/body_part_genres_controller.rb
@@ -1,14 +1,23 @@
 class Admin::BodyPartGenresController < ApplicationController
   def index
     @body_part_genres = BodyPartGenre.all
+    @body_part_genre = BodyPartGenre.new
   end
 
   def edit
+    @body_part_genre = BodyPartGenre.find(params[:id])
+  end
+
+  def update
+    @body_part_genre = BodyPartGenre.find(params[:id])
+    @body_part_genre = BodyPartGenre.update(body_part_genre_params)
+    redirect_to admin_body_part_genres_path
   end
 
   def create
-    @body_part_genre = BodyPartGenre.new
+    @body_part_genre = BodyPartGenre.new(body_part_genre_params)
     @body_part_genre.save
+    redirect_to  request.referer
   end
 
   private
